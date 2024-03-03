4f6844619e9691a7cfb7192904719272ff092f55
Edwin <edwin.nguim@gmail.com>
2024-03-03 08:09:34 -0500

    Icons

--- FashionSwipe/Components/Footer.js
+++ FashionSwipe/Components/Footer.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+
+import PersonIcon from "../assets/Icons/PersonIcon.svg";
+import SwipeIcon from "../assets/Icons/SwipeIcon.svg";
+import MessageBubbleIcon from "../assets/Icons/MessageBubble.svg";
 
 const Footer = ({ onPageChange }) => {
   return (
     <View style={styles.footer}>
       <TouchableOpacity onPress={() => onPageChange(1)} style={styles.button}>
-        <Text style={styles.buttonText}>Profile</Text>
+        <PersonIcon width={24} height={24} />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => onPageChange(2)} style={styles.button}>
-        <Text style={styles.buttonText}>Swipe</Text>
+        <SwipeIcon width={24} height={24} />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => onPageChange(3)} style={styles.button}>
-        <Text style={styles.buttonText}>Chat</Text>
+        <MessageBubbleIcon width={24} height={24} />
       </TouchableOpacity>
     </View>
   );
@@ -32,13 +36,7 @@ const styles = StyleSheet.create({
   button: {
     paddingHorizontal: 20,
     paddingVertical: 10,
-    backgroundColor: '#007bff',
     borderRadius: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
   },
 });
 
